adae621c316d1310d0ed117a3e80de7f5824617a
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-10 19:21:43 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -9,17 +9,17 @@ class String
   def question?
     self.end_with?("?")
   end
-  
+
   def exclamation?
     self.end_with?("!")
   end
-  
+
   def count_sentences
     self.split(/[.!?]/).reject {|x| x.empty?}.size
   end
-  
+
   def count_sentences_words
-    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
+    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size
 
   end
 end
